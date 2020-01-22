8db26d6058a5b778b8d3edca68dedf58b7f1ca9c
YOUR NAME <YOUR EMAIL ADDRESS>
2020-01-22 01:14:58 -0500

    Uploading bonus challenge

--- calculator.py
+++ calculator.py
@@ -10,6 +10,19 @@ def substract(a,b):
 def divide(a,b):
     return a / b
 
+def square(a):
+	return a ** 2
+
+def cube(a):
+	return a ** 3
+
+def square_n_times(a,n):
+	return a ** n
+
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
+print(x)
+
+print("I'm going use the calculator functions to square 5, 6 times")
+x = square_n_times(5,6)
 print(x)
