6517e3ba26154625f2148b7295be3aa0fe709c97
YOUR NAME <YOUR EMAIL ADDRESS>
2020-01-22 01:26:56 -0500

    Calling on and printing calculator functions but this time with js

--- calculator.js
+++ calculator.js
@@ -1,21 +1,30 @@
-function multiply(a,b):
+function multiply(a,b) {
     return a * b
+}
 
-function add(a,b):
+function add(a,b) {
     return a + b
+}
 
-function substract(a,b):
+function substract(a,b) {
     return a - b
+}
 
-function divide(a,b):
+function divide(a,b) {
     return a / b
-function square(a):
-	return a ** 2
-function cube(a):
-	return a ** 3
+}
 
-function square_n_times(a,n):
+function square(a) {
+	return a ** 2
+}
+
+function cube(a) {
+	return a ** 3
+}
+
+function square_n_times(a,n) {
 	return a ** n
+}
 
 console.log("I'm going use the calculator functions to multiply 5 and 6")
 var x = multiply(5,6)
